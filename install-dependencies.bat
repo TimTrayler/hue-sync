e1fdf34fe686dff16ca482ea28cae53746b11b06
Tim Trayler <67274738+TimTrayler@users.noreply.github.com>
2021-03-12 08:20:45 +0100

    Delete install-dependencies.bat

--- install-dependencies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pip install opencv-python
-pip install darkdetect
-pip install pyautogui
-pip install requests
-pip install tkinter
-pip install pygubu
-pip install rgbxy
-pip install numpy
